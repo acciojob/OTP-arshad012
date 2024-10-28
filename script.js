97c1e30e6be7e961e36e1a66f221ecd13d1e587b
arshad012 <forarshad99@gmail.com>
2024-10-29 02:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const allInputs = document.querySelectorAll('.code');
+
+allInputs.forEach((input, i) => {
+	input.oninput = () => {
+		// input.onkeydown = (event) => {
+		// 	if(event.key == 'Backspace' && i>0) {
+		// 		allInputs[i-1].focus();
+		// 		return;
+		// 	}
+		// }
+		if(input.value != '' && i < allInputs.length-1) {
+			allInputs[i+1].focus();
+		}
+		else if(input.value == '' && i > 0) {
+			allInputs[i-1].focus();
+		}
+	}
+})
+
+// window.addEventListener('keydown', (event) => {
+// 	console.log(event.key);
+// })
